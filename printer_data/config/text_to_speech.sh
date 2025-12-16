f916f6a1d392a138f588cf60dfbefded404e5d11
matt <matt@ender5plus-7955eb3>
2025-12-16 10:32:31 -0600

    text_to_speech.sh modified - 16/12/25 - 10:32:20

---
+++ printer_data/config/text_to_speech.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+MESSAGE="$*"
+curl -X POST \
+  -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI5Y2UxYTQzYWVjZGE0ODg3YjBiZDFiNjIwZGM0MjNmNiIsImlhdCI6MTc2NTg5ODA1NywiZXhwIjoyMDgxMjU4MDU3fQ.-YU7DjZyp3-Cdo4bZytk02fAEJ8QgfOC-ktrtOl8aYw" \
+  -H "Content-Type: application/json" \
+  -d "{\"entity_id\": \"input_text.printer_speech_text\", \"value\": \"$MESSAGE\"}" \
+  http://192.168.1.254:8123/api/services/input_text/set_value
